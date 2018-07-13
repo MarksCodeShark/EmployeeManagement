aa31f0f3347e29de435b20d1d185ff465e4dba78
Mark Coetzer <u14044537@tuks.co.za>
2018-07-13 17:56:30 +0200

    Created CSV Parser : EmployeeReader class - To read in employee data

---
+++ Source/Code/EmployeeReader.java
@@ -0,0 +1,20 @@
+/**
+ * @author  Mark Coetzer
+ * @version 1.0
+ * @since   2018-07-14 
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class EmployeeReader {
+    public static void read() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("../EmployeeList - Employees.csv"));
+        scanner.useDelimiter(",");
+        while(scanner.hasNext()){
+            System.out.print(scanner.next()+"|");
+        }
+        scanner.close();
+    }
+ }
