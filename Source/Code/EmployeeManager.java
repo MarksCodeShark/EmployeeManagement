eb8893be92b48934f8f0b8344ccf90389f7e0a78
Mark Coetzer <u14044537@tuks.co.za>
2018-07-17 07:33:46 +0200

    Added basic code to test the read function

--- Source/Code/EmployeeManager.java
+++ Source/Code/EmployeeManager.java
@@ -4,6 +4,8 @@
  * @since   2018-07-14 
  */
 
+import java.io.FileNotFoundException;
+
  public class EmployeeManager {
      
      /**
@@ -11,6 +13,10 @@
       * @param args The command line arguments
       */
      public static void main(String[] args) {
-         
+         try {
+            EmployeeReader.read("Employees.csv");
+         } catch (FileNotFoundException e) {
+            // TODO: Do something with file not found    
+         }
      }
  }
