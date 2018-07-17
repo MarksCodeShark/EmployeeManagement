804893ce12a4767ca51f74363ca170a98e4d1471
Mark Coetzer <u14044537@tuks.co.za>
2018-07-17 07:22:46 +0200

    Created EmployeeManager as the main entry point

---
+++ Source/Code/EmployeeManager.java
@@ -0,0 +1,16 @@
+/**
+ * @author  Mark Coetzer
+ * @version 1.0
+ * @since   2018-07-14 
+ */
+
+ public class EmployeeManager {
+     
+     /**
+      * TODO: Complete this JavaDoc Stub
+      * @param args The command line arguments
+      */
+     public static void main(String[] args) {
+         
+     }
+ }
